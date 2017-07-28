90e2af57a7def220d298ecac8ec6a1f77a45b6d3
Reekah Delfanian <krolochin@gmail.com>
2017-07-28 12:36:31 -0500

    Add files via upload
    Adding key listeners.

--- js/main.js
+++ js/main.js
@@ -9,8 +9,7 @@
 /* global AudioContext, SoundMeter */
 
 'use strict';
-
-var talkingPic = document.querySelector('#talkingpic div');
+//keycodes are 111 - divide - and 106 - multiply
 var instantMeter = document.querySelector('#instant meter');
 var slowMeter = document.querySelector('#slow meter');
 var clipMeter = document.querySelector('#clip meter');
@@ -19,7 +18,25 @@ var instantValueDisplay = document.querySelector('#instant .value');
 var slowValueDisplay = document.querySelector('#slow .value');
 var clipValueDisplay = document.querySelector('#clip .value');
 var lastPolledInstant= Date.now();
-var lastShoutedInstant;
+var lastShoutedInstant= Date.now();
+var expressing=false;
+
+// hook message constants
+var HOOK_MESSAGE_TYPE = {
+    WM_KEYDOWN    : 0x0100,
+    WM_KEYUP    : 0x0101,
+    WM_SYSKEYDOWN : 0x0104,
+    WM_SYSKEYUP   : 0x0105,
+    WM_LBUTTONDOWN  : 0x0201,
+    WM_LBUTTONUP  : 0x0202,
+    WM_MOUSEMOVE  : 0x0200,
+    WM_MOUSEWHEEL : 0x020A,
+    WM_MOUSEHWHEEL  : 0x020E,
+    WM_RBUTTONDOWN  : 0x0204,
+    WM_RBUTTONUP  : 0x0205,
+    WM_MBUTTONDOWN  : 0x0207,
+    WM_MBUTTONUP  : 0x0208
+};
 
 try {
     window.AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -51,24 +68,24 @@ function handleSuccess(stream) {
                 soundMeter.slow.toFixed(2);
             clipMeter.value = clipValueDisplay.innerText =
                 soundMeter.clip;
-            if (instantMeter.value < .03 && ((Date.now() - lastShoutedInstant > 1000))){
+            if (instantMeter.value < .03 && ((Date.now() - lastShoutedInstant > 1000))&& !expressing){
                 if ((Date.now() - lastPolledInstant) > 500){
                     document.getElementById('talkingPic')
                         .innerHTML = '<img src="idle.png" />';
                 }
             }
-            if (instantMeter.value > .03 && ((Date.now() - lastShoutedInstant > 1000))){
+            if (instantMeter.value > .03 && ((Date.now() - lastShoutedInstant > 1000)) && !expressing){
                 document.getElementById('talkingPic')
                     .innerHTML = '<img src="talking.png" />';
                 lastPolledInstant=Date.now();
             }
-            if (instantMeter.value > .4){
+            if (instantMeter.value > .4 && !expressing){
                 document.getElementById('talkingPic')
                     .innerHTML = '<img src="shouting.png" />';
                 lastPolledInstant=Date.now();
                 lastShoutedInstant=Date.now();
             }
-            if(((Date.now() - lastShoutedInstant) < 1000) && ((Date.now() - lastShoutedInstant) > 500)){
+            if(((Date.now() - lastShoutedInstant) < 1000) && ((Date.now() - lastShoutedInstant) > 500 ) && !expressing){
                 document.getElementById('talkingPic')
                     .innerHTML = '<img src="recovering.png" />';
                 lastPolledInstant=Date.now();
@@ -80,6 +97,59 @@ function handleSuccess(stream) {
 function handleError(error) {
     console.log('navigator.getUserMedia error: ', error);
 }
+
+external.LoadDll('Scriptdlls\\SplitmediaLabs\\XjsEx.dll');
+external.CallDllEx('xsplit.HookSubscribe');
+
+window.OnDllOnInputHookEvent = function(msg, wparam, lparam){
+    switch (parseInt(msg)) {
+        case HOOK_MESSAGE_TYPE.WM_KEYDOWN:
+        case HOOK_MESSAGE_TYPE.WM_SYSKEYDOWN:
+            console.log("Keystroke received: "+wparam);
+            if(wparam==111){
+                expressing=true;
+                document.getElementById('talkingPic')
+                    .innerHTML = '<img src="1.png" />';
+            }
+            if(wparam==106){
+                expressing=true;
+                document.getElementById('talkingPic')
+                    .innerHTML = '<img src="2.png" />';
+            }
+            break;
+        case HOOK_MESSAGE_TYPE.WM_KEYUP:
+        case HOOK_MESSAGE_TYPE.WM_SYSKEYUP:
+            if(wparam==111){
+                expressing=false;
+            }
+            if(wparam==106){
+                expressing=false;
+            }
+            break;
+    }
+};
+
+// function handleKeydown(wparam, lparam){
+//     if(wparam==111){
+//             expressing=true;
+//             document.getElementById('talkingPic')
+//                 .innerHTML = '<img src="1.png" />';
+//         }
+//     if(wparam==106){
+//         expressing=true;
+//         document.getElementById('talkingPic')
+//             .innerHTML = '<img src="2.png" />';
+//     }
+// }
+//
+// function handleKeyup(wparam, lparam){
+//     if(wparam==111){
+//         expressing=false;
+//     }
+//     if(wparam==106){
+//         expressing=false;
+//     }
+// }
 
 navigator.mediaDevices.getUserMedia(constraints).
 then(handleSuccess).catch(handleError);
